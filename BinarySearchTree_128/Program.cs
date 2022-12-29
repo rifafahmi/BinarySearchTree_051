0073592b2d4c577307168ac3205009688215b03b
Rifa <rifafahim21.com@gmial.com>
2022-12-29 17:05:01 +0700

    Add project files.

---
+++ BinarySearchTree_128/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BinarySearchTree_128
+{
+    class Program
+    {
+        /*A node class consi
+         * 
+         */
+        class node
+        {
+            public string info;
+            public node lchild;
+            public node rchild;
+            //constructor for the node class
+
+            public node(string i, node l, node r)
+            {
+                info = i;
+                lchild = l;
+                rchild = r;
+            }
+        }
+        
+    }
